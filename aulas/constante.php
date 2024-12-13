61318210af34828773dab8609bfcef25b8ef66b3
linhareseduardo <linhareseduardo7@gmail.com>
2024-12-13 11:17:22 -0300

    aula 05 constante

---
+++ aulas/constante.php
@@ -0,0 +1,18 @@
+<?php 
+
+    define('LANGUAGE', 'PT_BR');
+
+    var_dump( LANGUAGE );
+
+    echo '<br>';
+
+    var_dump(__FILE__);
+
+    echo '<br>';
+
+    var_dump(__LINE__);
+
+    echo '<br>';
+
+    var_dump(__DIR__);
+?>
